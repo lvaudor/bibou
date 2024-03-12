a5295d7a10a14aed799ae82cd0b0becf8471df0f
lvaudor <lise.vaudor@ens-lyon.fr>
2024-03-12 16:46:03 +0100

    change parameters to from_file, to_file

--- R/bib_merge_files.R
+++ R/bib_merge_files.R
@@ -1,14 +1,14 @@
 #' This function merges entries from several .bib files into a single .bib file
-#' @param bibtex_files the paths to the exported files
-#' @param bibtex_file the path to the single merged .bib file. Defaults to "savedrecs_merged.bib" the suffix to use to create new .bib file (the new file name will be bibtex_file_\{suffix\}). Defaults to "_clean"
+#' @param from_files the paths to the exported files
+#' @param to_file the path to the single merged .bib file. Defaults to "savedrecs_merged.bib" the suffix to use to create new .bib file (the new file name will be bibtex_file_\{suffix\}). Defaults to "_clean"
 #' @return nothing
 #' @examples
 #' bibtex_file_1=system.file("savedrecs(1).bib",package="bibou")
 #' bibtex_files=purrr::map_chr(as.character(1:5),~stringr::str_replace(bibtex_file_1,"1(?=\\).bib)",.x))
-#' bib_merge_files(bibtex_files)
+#' bib_merge_files(from_files=bibtex_files,to_file="data-raw/savedrecs_merged.bib")
 #' @export
 
-bib_merge_files <- function(bibtex_files, bibtex_file="savedrecs_merged.bib") {
-  bibtex_data=purrr::map(bibtex_files,readLines) %>% unlist()
-  writeLines(bibtex_data,con = bibtex_file)
+bib_merge_files <- function(from_files, to_file="savedrecs_merged.bib") {
+  bibtex_data=purrr::map(from_files,readLines) %>% unlist()
+  writeLines(bibtex_data,con = to_file)
 }
--- R/bib_remove_duplicates.R
+++ R/bib_remove_duplicates.R
@@ -1,12 +1,12 @@
 #' This function removes entries with the same key in a .bib file and writes a new .bib file
-#' @param bibtex_file the path to the .bib file
-#' @param suffix the suffix to use to create new .bib file (the new file name will be bibtex_file_\{suffix\}). Defaults to "_clean"
+#' @param from_file the path to the .bib file
+#' @param to_file the file with no duplicates created
 #' @return nothing
 #' @examples
 #' @export
 
-bib_remove_duplicates <- function(bibtex_file, suffix="_clean") {
-  bibtex_data <- readLines(bibtex_file)
+bib_remove_duplicates <- function(from_file, to_file="savedrecs_clean.bib") {
+  bibtex_data <- readLines(from_file)
   # Create tibble with 1 line = 1 bib entry
   tib=tibble::tibble(
     # Detect first and last lines of bib entries
@@ -18,18 +18,14 @@ bib_remove_duplicates <- function(bibtex_file, suffix="_clean") {
   ) %>%
     dplyr::group_by(key) %>%
     # For each key keep only lines corresponding to first bib entry
-    dplyr::summarise(firstl=max(first(firstl)-1,1),
-              lastl=first(lastl),
-              .groups="drop")
-
-  bibtex_file_clean=stringr::str_replace(bibtex_file,
-                                         ".bib",
-                                         paste0(suffix,".bib"))
+    dplyr::summarise(firstl=max(dplyr::first(firstl)-1,1),
+                     lastl=dplyr::first(lastl),
+                     .groups="drop")
   lines=c()
   for (i in 1:nrow(tib)){
     lines=c(lines,
             tib$firstl[i]:tib$lastl[i])
   }
   result=bibtex_data[lines]
-  writeLines(bibtex_data[lines],con = bibtex_file_clean)
+  writeLines(bibtex_data[lines],con = to_file)
 }
